f24ecab95a71929ab5c463a9004ce48db877d225
nobusada_suzuki <nobusada.suzuki.likeit@gmail.com>
2018-12-06 00:26:12 +0900

    1-7

--- Practice/src/practice01/PTra01_07.java
+++ Practice/src/practice01/PTra01_07.java
@@ -21,7 +21,14 @@ public class PTra01_07 {
 
 		// ★ それぞれの型変数に、値を代入してください
 		// ※ 値は、好きな数字を入力してください
-
+		bo = true;
+		by = 1;
+		s = 10;
+		c = 'あ';
+		i = 100;
+		l = 1000L;
+		f = 0.1f;
+		d = 0.1;
 
 
 
@@ -31,6 +38,14 @@ public class PTra01_07 {
 
 
 		// ★ それぞれの変数の中身を出力してください
+		System.out.println(bo);
+		System.out.println(by);
+		System.out.println(s);
+		System.out.println(c);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
 
 
 
