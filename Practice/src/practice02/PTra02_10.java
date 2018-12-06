0506ddacd048de9b033962044382be1611a4aa82
nobusada_suzuki <nobusada.suzuki.likeit@gmail.com>
2018-12-06 15:45:13 +0900

    2-10

--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,8 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
-
+			double tax = 1.08;
+			moneyInHand = (int) (moneyInHand - (((coffee*2)+snack)*tax));
+		System.out.println(moneyInHand);
 	}
 }
