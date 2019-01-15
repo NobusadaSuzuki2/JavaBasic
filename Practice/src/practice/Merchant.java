5bfd481bcfa35c0b4f869e5804bf8725000d8f3e
nobusada_suzuki <nobusada.suzuki.likeit@gmail.com>
2019-01-15 15:46:01 +0900

    10-6

--- Practice/src/practice/Merchant.java
+++ Practice/src/practice/Merchant.java
@@ -1,17 +1,22 @@
 package practice;
 
 public class Merchant {
-	
+	/*商人は「商品の値段(price)」 、「在庫(stock)」、「売上(money)」という属性を持っています。
+	商品の値段は100、在庫は100、 売上は0 で初期化してください。そのうち商品の値段は定数で宣言してください。
+	商人は「商品を売る」(sell)と、売上を確認する(confirm)という行動を取ることができます。
+	*/
+
 	final int PRICE=100;
 	int stock=100;
 	int money=0;
-	//属性
 	
+	//商人は以下の操作(メソッド)を持っています。
+	//商品を売る(sellItem)：渡された引数(整数)の数だけ在庫が減り、売上が「商品の値段×引数」分増えます。戻り値はありません。
 	void sellItem(int count) {
 		this.stock-= count;
 		this.money+= this.PRICE*count;
 	}
-	
+	//売上を確認する(confirmMoney)：引数なしで、戻り値として「売上は○○円です。」という文字列を返す。○○には売上が入ります。
 	String confirmMoney() {
 		return"売り上げは"+this.money+"円です。";
 	}
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -18,6 +18,52 @@ public class PTra10_06 {
 		 * carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * 各carインスタンスのrunメソッドを実行して、それぞれ「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください。
 		 */
+		Car car1=new Car();
+		car1.color="RED";
+		car1.gasoline=50;
+		
+		int run1 = 0;
+		for(int i=1;i<=100;i++) {
+			run1 += car1.run();
+			if(car1.gasoline<=0) {
+				System.out.println(car1.color+"は目的地に到達できませんでした");
+				break;
+			}else if(run1>=300) {
+				System.out.println(car1.color+"は目的地にまで"+i+"時間かかりました。残りのガソリンは、"+car1.gasoline+"リットルです");
+				break;
+			}
+		}
+		Car car2=new Car();
+		car2.color="BLUE";
+		car2.gasoline=50;
+		
+		int run2 =0;
+		for(int i=1;i<=100;i++) {
+			run2 += car2.run();
+			if(car2.gasoline<=0) {
+				System.out.println(car2.color+"は目的地に到達できませんでした");
+				break;
+			}else if(run2>=300) {
+				System.out.println(car2.color+"は目的地にまで"+i+"時間かかりました。残りのガソリンは、"+car2.gasoline+"リットルです");
+				break;
+			}
+		}
+		
+		Car car3=new Car();
+		car3.color="yellow";
+		car3.gasoline=50;
+		
+		int run3=0;
+		for(int i=1;i<=100;i++) {
+			run3 += car3.run();
+			if(car3.gasoline<=0) {
+				System.out.println(car3.color+"は目的地に到達できませんでした");
+				break;
+			}else if(run3>=300) {
+				System.out.println(car3.color+"は目的地にまで"+i+"時間かかりました。残りのガソリンは、"+car3.gasoline+"リットルです");
+				break;
+			}
+		}
 
 	}
 }
