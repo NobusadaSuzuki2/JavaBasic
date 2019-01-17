3e18e23550dc030ae8195ea6fc31923913c2e8b0
nobusada_suzuki <nobusada.suzuki.likeit@gmail.com>
2019-01-17 16:24:14 +0900

    Update PTra11_02.java

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -1,4 +1,9 @@
 package practice11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
  * PTra11_02.java
  *   作成	LIKEIT	2017
@@ -7,14 +12,19 @@ package practice11;
  */
 
 public class PTra11_02 {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
-
+		FileReaderClass.readBookDataFile();
+		
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
-
+		BufferedReader d=new BufferedReader(new InputStreamReader(System.in));
+		String title;
+	    title=d.readLine();
+	    if(title.equals(FileReaderClass.readBookDataFile())){
+	      System.out.println(title);
+	    }
 	}
 }
