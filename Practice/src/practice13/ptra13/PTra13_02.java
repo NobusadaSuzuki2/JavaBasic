32ad56e77dca7c06e5c0d0a8ca8c7c1a9785e436
nobusada_suzuki <nobusada.suzuki.likeit@gmail.com>
2019-01-22 18:05:47 +0900

    13-2

--- Practice/src/practice13/ptra13/PTra13_02.java
+++ Practice/src/practice13/ptra13/PTra13_02.java
@@ -6,8 +6,9 @@
  */
 package practice13.ptra13;
 
-public class PTra13_02 {
+import practice13.common.Character;
 
+public class PTra13_02 {
 	/*
 	 * ★ common.Characterクラスを継承した、common.Heroクラスを作成してください
 	 * 	●HP, POWER, ENDURANCEはそれぞれ、（25, 10, 7）
@@ -15,12 +16,18 @@ public class PTra13_02 {
 
 	public static void main(String[] args) {
 		// ★ common.Heroクラスをインスンタンス化してください
-
+		Hero hero=new Hero(25,10,7);
 
 		// ★ common.Heroインスタンスのnameに"勇者"を設定して下さい
-
+		hero.setName("勇者");
 
 		// ★ common.Heroインスタンスのキャラクター情報を表示してください
+		System.out.println(hero.showParameter());
+	}
+}
+class Hero extends Character{
 
+	public Hero(int hp, int power, int endurance) {
+		super(hp, power, endurance);
 	}
 }
